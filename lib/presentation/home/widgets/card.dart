1f91652cfd1962ef432304046698af31a6dddc6f
Anas Fikhi <work@gwhyyy.com>
2023-05-16 08:34:59 +0100

    [ Add ] adde a sample of post card

--- lib/presentation/home/widgets/card.dart
+++ lib/presentation/home/widgets/card.dart
@@ -10,6 +10,51 @@ class SoltanaPostCard extends StatelessWidget {
   final SoltanaPost post;
   @override
   Widget build(BuildContext context) {
-    return Text(post.title);
+    return Container(
+      clipBehavior: Clip.hardEdge,
+      margin: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Colors.grey.shade200,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.center,
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: Image.network(post.imagePath),
+              ),
+              Positioned(
+                bottom: -10,
+                right: 0,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 2.0, horizontal: 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.circular(4.0),
+                  ),
+                  child: Text(
+                    "الرئيسية",
+                    style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                          color: Colors.white,
+                        ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 15.0),
+          Text(
+            post.title,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 15.0),
+        ],
+      ),
+    );
   }
 }
