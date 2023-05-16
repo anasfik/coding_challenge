36a4efd8047b449a6017b4de28ba8536c0e332f1
Anas Fikhi <work@gwhyyy.com>
2023-05-16 08:56:06 +0100

    [ Add ] adde a full screen of soltana cards

---
+++ lib/presentation/card_full_screen/card_full_screen.dart
@@ -0,0 +1,15 @@
+import 'package:coding_challenge_app/data/models/soltana_model.dart';
+import 'package:flutter/material.dart';
+
+class SoltanaPostFullScreen extends StatelessWidget {
+  const SoltanaPostFullScreen({
+    super.key,
+    required this.post,
+  });
+
+  final SoltanaPost post;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/presentation/home/widgets/card.dart
+++ lib/presentation/home/widgets/card.dart
@@ -2,6 +2,8 @@ import 'package:coding_challenge_app/data/models/soltana_model.dart';
 import 'package:coding_challenge_app/utils/extensions.dart';
 import 'package:flutter/material.dart';
 
+import '../../card_full_screen/card_full_screen.dart';
+
 class SoltanaPostCard extends StatelessWidget {
   const SoltanaPostCard({
     super.key,
@@ -11,67 +13,77 @@ class SoltanaPostCard extends StatelessWidget {
   final SoltanaPost post;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      clipBehavior: Clip.hardEdge,
-      margin: const EdgeInsets.all(8.0),
-      decoration: const BoxDecoration(color: Colors.transparent),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Stack(
-            clipBehavior: Clip.none,
-            alignment: Alignment.center,
-            children: <Widget>[
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8.0),
-                child: Image.network(
-                  post.imagePath,
-                  width: double.infinity,
-                  height: 200.0,
-                  fit: BoxFit.fitWidth,
-                  loadingBuilder: (context, child, loadingProgress) {
-                    return Container(
-                      height: 200.0,
-                      width: double.infinity,
-                      color: Colors.grey.shade300,
-                    );
-                  },
-                ),
-              ),
-              Positioned(
-                bottom: -10,
-                right: 0,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 2.0, horizontal: 8.0),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(4.0),
-                  ),
-                  child: Text(
-                    "الرئيسية",
-                    style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                          color: Colors.white,
-                        ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => SoltanaPostFullScreen(post: post),
+          ),
+        );
+      },
+      child: Container(
+        clipBehavior: Clip.hardEdge,
+        margin: const EdgeInsets.all(8.0),
+        decoration: const BoxDecoration(color: Colors.transparent),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Stack(
+              clipBehavior: Clip.none,
+              alignment: Alignment.center,
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.network(
+                    post.imagePath,
+                    width: double.infinity,
+                    height: 200.0,
+                    fit: BoxFit.fitWidth,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      return Container(
+                        height: 200.0,
+                        width: double.infinity,
+                        color: Colors.grey.shade300,
+                        child: child,
+                      );
+                    },
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 15.0),
-          Text(
-            post.title,
-            style: Theme.of(context).textTheme.titleLarge,
-          ),
-          const SizedBox(height: 5.0),
-          Text(
-            post.date!.arabicFormatted,
-            style: Theme.of(context).textTheme.labelMedium?.copyWith(
-                  color: Colors.grey.shade600,
+                Positioned(
+                  bottom: -10,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 2.0, horizontal: 8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: Text(
+                      "الرئيسية",
+                      style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
+                  ),
                 ),
-          ),
-          const SizedBox(height: 15.0),
-        ],
+              ],
+            ),
+            const SizedBox(height: 15.0),
+            Text(
+              post.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 5.0),
+            Text(
+              post.date!.arabicFormatted,
+              style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                    color: Colors.grey.shade600,
+                  ),
+            ),
+            const SizedBox(height: 15.0),
+          ],
+        ),
       ),
     );
   }
